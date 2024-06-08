0cc9110d9322d69c38fa44cd4f1865b18c84e9ff
HolbieEnjoyer <8698@holbertonstudents.com>
2024-06-08 16:34:04 +0000

    Next task

--- python-serialization/ilahe/holbertonschool-higher_level_programming/python-if_else_loops_functions/4-print_hexa.py
+++
@@ -1,3 +0,0 @@
-#!/usr/bin/python3
-for i in range(99):
-    print("{0}".format(f"{str(i)} = {hex(i)}"))
--- python-serialization/ilahe/holbertonschool-higher_level_programming/python-if_else_loops_functions/9-print_last_digit.py
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/python3
-def print_last_digit(number):
-    l_number = abs(number) % 10
-    print("{}".format(l_number), end='')
-    return l_number
