0492f53cb6f69447e35f7561ef2e072434dc3f88
HolbieEnjoyer <8698@holbertonstudents.com>
2024-06-25 20:28:52 +0400

    Next task

---
+++ SQL_more_queries/12-no_genre.sql
@@ -0,0 +1,6 @@
+-- hi
+SELECT DISTINCT tv_shows.title, tv_show_genres.genre_id
+FROM tv_shows
+JOIN tv_show_genres ON tv_shows.id = tv_show_genres.show_id
+WHERE tv_shows.genre_id is NULL
+ORDER BY tv_shows.title, tv_show_genres.genre_id ASC;
