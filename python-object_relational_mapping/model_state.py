13d2fcb51d03056a05d69841f278c504abd74316
HolbieEnjoyer <8698@holbertonstudents.com>
2024-06-27 22:52:34 +0400

    Next task

--- python-object_relational_mapping/model_state.py
+++ python-object_relational_mapping/model_state.py
@@ -12,12 +12,6 @@ Base = declarative_base()
 
 class State(Base):
     def __init__(self, id, name):
-        """Represents a state for a MySQL database.
-
-        __tablename__ (str): The name of the MySQL table to store States.
-        id (sqlalchemy.Integer): The state's id.
-        name (sqlalchemy.String): The state's name.
-        """
         __tablename__ = 'states'
         id = Column(Integer, primary_key=True, nullable=False)
         name = Column(String(128), nullable=False)
