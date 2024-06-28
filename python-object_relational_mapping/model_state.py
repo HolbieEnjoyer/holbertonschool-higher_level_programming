2be8bbcb0f54f3bd84fbf24c410259faf2918163
HolbieEnjoyer <8698@holbertonstudents.com>
2024-06-28 11:32:45 +0400

    Next task

--- python-object_relational_mapping/model_state.py
+++ python-object_relational_mapping/model_state.py
@@ -4,13 +4,14 @@ This script connects to a MySQL database
 """
 
 from sqlalchemy import Column, Integer, String
-from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import relationship
+from sqlalchemy.ext.declarative import declarative_base
 
 Base = declarative_base()
 
 
 class State(Base):
+    """A class that represents a state"""
     __tablename__ = 'states'
     id = Column(Integer, primary_key=True, nullable=False,
                 autoincrement=True, unique=True)
