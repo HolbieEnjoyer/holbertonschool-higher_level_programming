67d43e4152b4587c68e8cd14823242922483f694
HolbieEnjoyer <8698@holbertonstudents.com>
2024-06-28 13:39:28 +0400

    Next task

--- python-object_relational_mapping/model_city.py
+++ python-object_relational_mapping/model_city.py
@@ -3,17 +3,16 @@
 This script connects to a MySQL database
 """
 
-from sqlalchemy import Column, Integer, String
-from sqlalchemy.orm import relationship
+from sqlalchemy import Column, Integer, String, ForeignKey
 from sqlalchemy.ext.declarative import declarative_base
-from model_city import City
 
 Base = declarative_base()
 
 
-class State(Base):
+class City(Base):
     """A class that represents a state"""
-    __tablename__ = 'states'
+    __tablename__ = 'cities'
     id = Column(Integer, primary_key=True, nullable=False,
                 autoincrement=True, unique=True)
+    state_id = Column(Integer, ForeignKey('states.id'), nullable=False)
     name = Column(String(128), nullable=False)
